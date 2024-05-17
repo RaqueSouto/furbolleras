c9fc2703ec4b1a0bc40460a7a11da685d75ae6e8
RaqueSouto <76965176+RaqueSouto@users.noreply.github.com>
2024-05-17 23:18:40 +0200

    cambios en FAQ y Sección Equipo en version movil

--- furbolleras_web/js/script.js
+++ furbolleras_web/js/script.js
@@ -1,8 +1,38 @@
 const menuBtn = document.querySelector('.Header-btn'); //variable del botón
 const menu = document.querySelector('.Header-menu'); //variable del menú
+const btnIzq = document.querySelector('.Btn-izq'); //variable botón equipo izq
+const btnDcha = document.querySelector('.Btn-dcha'); //variable botón equipo dcha
+const item = document.querySelectorAll('.Section-item'); //variable del item del equipo
+
 
 //Desplegar el menú hamburguesa
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle("isActive");
 });
 
+
+//Cambiar de jugadora en sección Equipo (versión móvil)
+let position = 0;
+
+let mostrarImg = function () {
+    item.forEach(function(each, i){
+        item[i].classList.remove('isActive');
+    })
+    item[position].classList.add('isActive');
+}
+
+
+btnDcha.addEventListener('click', () => {
+    position++;
+    if(position >= 16){
+        position = 0;
+    }
+    mostrarImg();
+});
+btnIzq.addEventListener('click', () => {
+    position--;
+    if(position < 0) {
+        position = 15;
+    }
+    mostrarImg();
+});
